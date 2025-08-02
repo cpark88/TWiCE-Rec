de60bd6b04550d61b8ae8cedba20c32317d02368
cpark88 <cpark88kr@gmail.com>
2025-08-02 12:04:33 +0000

    ignore Jupyter notebook checkpoints

--- .ipynb_checkpoints/sft-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-# Change batch size, number of epochs etc
-CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file recipes/accelerate_configs/zero2.yaml src/open_r1/sft.py \
-    --config recipes/CP_001/sft/sft_config_demo.yaml #zero2
-
-#lora merge save    
-python src/open_r1/model_save.py --config recipes/CP_001/sft/sft_config_demo.yaml
